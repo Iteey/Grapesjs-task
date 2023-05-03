235b63bb159d8b6999cf50443503910195b4949e
Denis Lesnikov <102676229+Iteey@users.noreply.github.com>
2023-05-04 01:01:56 +0300

    version 1.0.1
    more blocks added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from "react";
 import grapesjs from "grapesjs";
 import gjsPresetWebpage from "grapesjs-preset-webpage";
+import gjsPresetNewsletter from "grapesjs-preset-newsletter"
 import "./styles/main.scss";
 function App() {
   const [editor, setEditor] = useState(null);
   useEffect(() => {
     const editor = grapesjs.init({
       container: "#editor",
-      plugins:[gjsPresetWebpage],
+      plugins:[gjsPresetWebpage,gjsPresetNewsletter],
       pluginsOpts:{
         gjsPresetWebpage:{},
+        gjsPresetNewsletter:{},
       }
     });
     setEditor(editor);
